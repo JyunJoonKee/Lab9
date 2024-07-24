87c34d04f5606e38f543cfb2cd5bf983e8ae2ab1
alexatorr <torresa2@ufl.edu>
2024-07-24 18:27:45 -0400

    decoded and added something to your encode

--- main.py
+++ main.py
@@ -5,5 +5,22 @@ def encode(password):
     listofnumbers = list(password)
     for nums in range(len(listofnumbers)):
         addnum = int(listofnumbers[nums]) + 3
+        if addnum>+10:
+            addnum=addnum-10
         threestring += str(addnum)
     return threestring
+#Alexa's Decoding
+def decode(password):
+    substring=''
+    listnum=list(password)
+    for i in range (len(listnum)):
+        sub=int(listnum[i])-3
+        if sub<0:
+            sub=sub+10
+        substring= substring+str(sub)
+    return substring
+
+print(encode('12345555'))
+print(encode('00009962'))
+print (decode('45678888'))
+print(decode('33332295'))
